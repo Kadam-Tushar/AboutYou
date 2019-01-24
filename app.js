0b266d7aa4280cebc4e7086227eae6809ee1f890
Kadam-Tushar <tushar.kadam@somaiya.edu>
2019-01-24 12:05:26 +0530

    server added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener("load",(e) => {
     console.log("called");
 	if ('serviceWorker' in navigator) {
 		try {
-			navigator.serviceWorker.register('./serviceworker.js');
+			navigator.serviceWorker.register('./serverworker.js');
 			console.log('SW registered');
 		} catch (error) {
 			console.log('SW failed');
